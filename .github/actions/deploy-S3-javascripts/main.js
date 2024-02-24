dd9164038b1d59d9ee8c803c5bd5360f8c122a73
Phuc Nguyen <nguyenhuuphuc.yahoo@gmail.com>
2024-02-24 20:11:36 +0700

    fix dir required

--- .github/actions/deploy-S3-javascripts/main.js
+++ .github/actions/deploy-S3-javascripts/main.js
@@ -6,7 +6,7 @@ function run()   {
     // Get the inputs from the workflow file
     const bucket = core.getInput('bucket', {required: true});
     const bucketRegion = core.getInput('bucket-region', {required: false});
-    const dir = core.getInput('dist-dir', {required: false});
+    const dir = core.getInput('dist-dir', {required: true});
     // Upload the files
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${dir} ${s3Uri} --region ${bucketRegion}`);
