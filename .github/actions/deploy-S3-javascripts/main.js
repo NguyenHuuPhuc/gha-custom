0c3c5cabb044a8ad619fe423326f37d6f1c9dd9a
Phuc Nguyen <nguyenhuuphuc.yahoo@gmail.com>
2024-02-24 20:53:39 +0700

    print uplodad uri

--- .github/actions/deploy-S3-javascripts/main.js
+++ .github/actions/deploy-S3-javascripts/main.js
@@ -9,11 +9,8 @@ function run()   {
     const dir = core.getInput('dist-dir', {required: false});
     // Upload the files
     const s3Uri = `s3://${bucket}`;
-    exec.exec(`aws s3 sync ${dir} ${s3Uri} --region ${bucketRegion}`);
-
-
-    core.notice('Hello from my custom github actions');
-    
+    core.notice(`Uploading files from ${dir} to ${s3Uri} in ${bucketRegion}`);
+    exec.exec(`aws s3 sync ${dir} ${s3Uri} --region ${bucketRegion}`);    
 }
 
 run();
